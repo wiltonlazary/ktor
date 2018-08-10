5181c18287656679c2abdce2a6caf213e9325acd
Leonid Stashevsky <leonid.stashevsky@gmail.com>
2018-08-10 11:23:22 +0300

    Prepare js tests(compilation muted until compiler fix)

---
+++ ktor-client/ktor-client-js/test/io/ktor/client/engine/js/test/RequestTest.kt
@@ -0,0 +1,25 @@
+package io.ktor.client.engine.js.test
+
+import io.ktor.client.*
+import io.ktor.client.request.*
+import io.ktor.client.response.*
+import io.ktor.http.*
+import kotlinx.coroutines.experimental.*
+import kotlin.js.*
+import kotlin.test.*
+
+class RequestTest {
+
+//    @Test
+//    fun simpleRequestTest(): Promise<Unit> = async {
+//        val client = HttpClient()
+//
+//        val response: HttpResponse = client.get("https://google.com")
+//
+//        assertTrue(response.status.isSuccess())
+//        val text = response.readText()
+//        println(text)
+//
+//    }.asPromise()
+
+}
